414d89e4c1c202e503611fd5c16613fb0b6e50b8
Aravind_Aji <aravindaji@citymapia.in>
2022-12-28 18:44:42 +0530

    dec 27 merge

--- lib/main.dart
+++ lib/main.dart
@@ -39,7 +39,7 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
 
-  final String title;
+  final String  title;
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
